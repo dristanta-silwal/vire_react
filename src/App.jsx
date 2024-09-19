c639f865f452c828e12acd891483539c7bba655c
Dristanta Silwal <dristantasilwal003@gmail.com>
2024-09-18 19:56:52 -0700

    vite+react setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <button className="bg-blue-500 hover:bg-slate-600">
+        afdsfa
+      </button>
+    </>
+  )
+}
+
+export default App
